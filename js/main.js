19f8bf11cca0352a6d637f41622b049d6e31477a
kimhyeonjeong <ortg123@naver.com>
2024-01-19 17:59:33 +0900

    2024-01-19

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    $(".select_language").click(function(){
+        $(this).toggleClass("show")
+    })/* click */
+
+
+    $(".sub").hide();
+    // sub의 플렉스구조 전부다 가려주기 디스플레이 논
+
+    $(".gnb_menu > li").hover(function(){
+        $(this).find(".sub").stop().slideDown();
+        $(".gnb_menu").stop().animate({height:"80px"},500)
+    },function(){
+        $(this).find(".sub").stop().slideUp();
+        $(".gnb_menu").stop().animate({height:"50px"},500)
+    })//menu hover
+
+
+
+
+})//
